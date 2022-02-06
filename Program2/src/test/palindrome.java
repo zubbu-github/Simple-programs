500ef1e9f0ab1192c4281632bdf12b2c34286b0c
LENOVO <mdzubeir2468@gmail.com>
2022-02-06 23:13:34 +0530

    Palindrome or not

---
+++ Program2/src/test/palindrome.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.Scanner;
+
+public class palindrome {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);  
+	       System.out.println("Enter the number: ");  
+	       int num=sc.nextInt();
+	       int r,sum=0;
+	       int temp=num;    
+	       while(num>0)
+	       {    
+	       r=num%10;    
+	       sum=(sum*10)+r;    
+	       num=num/10;    
+	       }    
+	        if(temp==sum)    
+	        System.out.println("The entered number "+temp+" is a palindrome number ");    
+	        else    
+	        System.out.println("The entered number "+temp+" is not a palindrome");
+
+	}
+
+}
