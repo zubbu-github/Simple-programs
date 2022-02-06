c1902adcf2255ecfcb87f461316a0c6800066551
LENOVO <mdzubeir2468@gmail.com>
2022-02-06 23:50:34 +0530

    vowels or consonants

---
+++ Program2/src/test/vowelsconsonents.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.util.Scanner;
+
+public class vowelsconsonents {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a character :");
+	      Scanner sc = new Scanner(System.in);
+	      char ch = sc.next().charAt(0);
+	      if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == ' '){
+	         System.out.println("Given character is an vowel");
+	      }else{
+	         System.out.println("Given character is a consonant");
+	      }
+	}
+
+}
